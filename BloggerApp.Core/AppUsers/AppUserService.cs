2905284d5ed13efb42da188db03496174f4fa952
Ciaran <ciaranselwyn@yahoo.co.nz>
2019-05-11 23:31:40 +1200

    updated project structure

---
+++ BloggerApp.Core/AppUsers/AppUserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloggerApp.Core.AppUsers
+{
+    public interface IAppUserService
+    {
+
+    }
+
+    public class AppUserService : IAppUserService
+    {
+
+    }
+}
